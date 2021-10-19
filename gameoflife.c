b4f2c026e993861f11d234d9d4ee6bb5bafd496b
destrex271 <destrex271@gmail.com>
2021-10-19 10:16:27 +0530

    3rd commit: Bug fixes

--- gameoflife.c
+++ gameoflife.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
-
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
 #define GENNUMBER 1
 #define ROW 8
 #define COL 8
+#define UPPER 1
+#define LOWER 0
 
 int cells[8][8];
+bool lonlinessCheck(int row, int col);
+bool overcrowdCheck(int row, int col);
+bool aliveCheck(int row, int col);
+bool isToReincarnate(int row, int col);
+
 
 void populate(){
 	for(int i = 0; i < ROW; i++){
-		for(int j = 0; j < COL; j++){
-			cell[i][j] = (j%2); // Populating the cells with 0's and 1's where 1 stands for alive while 0 stands for dead
+		cells[i][0] = 0;
+		cells[i][COL-1] = 0;
+	}
+	for(int j = 0; j < ROW; j++){
+		cells[0][j] = 0;
+		cells[ROW-1][j] = 0;
+	}
+	for(int i = 1; i < ROW-1; i++){
+		for(int j = 1; j < COL-1; j++){
+			cells[i][j] = (rand()%(UPPER-LOWER+1))+LOWER; // Populating the cells with 0's and 1's where 1 stands for alive while 0 stands for dead
 		}
+	}
+}
+
+void display(){
+	for(int i = 0; i < ROW; i++){
+		for(int j = 0; j < COL; j++){
+			printf("%3d ", cells[i][j]);
+		}
+		printf("\n");
 	}
 }
 
@@ -18,8 +44,12 @@ void main(){
 
 	for(int gen = 0; gen < GENNUMBER; gen++){
 		populate();
-		for(int i = 0; i < ROw; i++){
-			for(int j = 0; j < COL; j++){
+		printf("GEN %d\n",gen);
+		display();
+		printf("----------");
+		for(int i = 1; i < ROW-1; i++){
+			printf("Iter %d\n",i);
+			for(int j = 1; j < COL-1; j++){
 				if(cells[i][j] == 1){
 					if(lonlinessCheck(i,j)){
 						cells[i][j] = 0;
@@ -35,6 +65,7 @@ void main(){
 					}
 				}
 			}
+			display();
 		}
 	}
 
@@ -43,23 +74,8 @@ void main(){
 bool lonlinessCheck(int row, int col){
 	bool isDead = true;
 	int liveCount = 0;
-	int rcount = row;
-	int colcount = col;
-	if(i == 0){
-		for (int i = 0 ; i <= ; i++){
-                for(int j = col - 1; j <= col; j++){//Checking the cells surrounding the given cell for alive members
-                        if(cells[i][j] != 0){
-                                liveCount++;
-                        }
-                        if(liveCount > 1){
-                                isDead = false;
-                                break;
-                        }
-                }
-       	 }
-	}
 	for (int i = row - 1; i <= row+1; i++){
-		for(int j = col - 1; j <= col; j++){//Checking the cells surrounding the given cell for alive members
+		for(int j = col - 1; j <= col+1; j++){//Checking the cells surrounding the given cell for alive members
 			if(cells[i][j] != 0){
 				liveCount++;
 			}
